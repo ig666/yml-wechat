f8b0198ee94b9bf4ad724a35a4083fe9e8d0c3db
Qi Chen <qi.chen>
2021-10-31 01:24:34 +0800

    feat: 牙美啦小程序基本定稿

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,13 @@
-import { View } from "@tarojs/components";
+import { View, Image } from "@tarojs/components";
 import "./index.less";
 
 const Home = () => {
-  return <View className='home'>首页</View>;
+  return (
+    <View className='home'>
+      <Image className='devlping' src='https://yameila.oss-cn-beijing.aliyuncs.com/home/62474-developing.gif'></Image>
+      <View className='dev-name'>暂未开放...</View>
+    </View>
+  );
 };
 
 export default Home;
