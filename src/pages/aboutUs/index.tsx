b768052d321293ee11378d290dc4f2c0899b2ad7
chenqi <ig666>
2021-12-20 09:58:07 +0800

    fix: 修改关于我们海报图

--- src/pages/aboutUs/index.tsx
+++ src/pages/aboutUs/index.tsx
@@ -3,7 +3,7 @@ import "./index.less";
 
 const AboutUs = () => {
   return (
-    <Image className='about-us' src='https://yameila.oss-cn-beijing.aliyuncs.com/home/%E8%87%AA%E6%88%91%E4%BB%8B%E7%BB%8D.jpg?versionId=CAEQFhiBgICCouvM7hciIDY1ZTBjZDc0ZWVjZDQyZTliNTAyMTY0YTA3NDlhMzAz' />
+    <Image className='about-us' src='https://yameila.oss-cn-beijing.aliyuncs.com/home/%E8%87%AA%E6%88%91%E4%BB%8B%E7%BB%8D.jpg' />
   );
 };
 
